274e774d7d67efc50bfdf81503d2e996e48237f5
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-11-12 12:02:29 -0500

    shaders working

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
 	// shaderback.setDebug(true);
-	// document.onLoad = shaderback.loadURL("js/shader.txt");
+	document.onLoad = shaderback.loadURL("js/shader.txt");
 
 });
